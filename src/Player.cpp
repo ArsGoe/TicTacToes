c93dd39179a6902cb5f679d4050995e170913a81
ROMÉO JACQUEMIN <romeo.jacquemin@nomachine.univ-eiffel.fr>
2025-12-04 10:15:06 +0100

    ajout de la definition de la fonction 'create_player'

---
+++ src/Player.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "Player.hpp"
+#include <string> // for string class
+using namespace std;
+
+Player::player create_player() {
+    Player::player player_construct;
+
+    cout << "Quel est votre pseudo ?" << std::endl;
+    cin >> player_construct.name;
+
+    cout << "Quel est le symbole que vous utiliserez ?" << std::endl;
+    cin >> player_construct.symbol;
+
+    return player_construct;
+}
